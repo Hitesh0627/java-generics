d6759aae6a37106b6bc40780432a60f7e0dedfaf
Hitesh0627 <103327522+Hitesh0627@users.noreply.github.com>
2025-03-28 13:52:59 +0530

    [Hitesh] ADD: Implemented Meal Plan structure for diet planning

---
+++ com/bridgelabz/mealPlanGenerator/MealPlan.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.mealPlanGenerator;
+
+//Interface for all meal types
+public interface MealPlan {
+    void displayMeal();
+}
+
+
+//Meal types
+
+//Vegetarian Meal
+class VegetarianMeal implements MealPlan {
+    @Override
+    public void displayMeal() {
+        System.out.println("Vegetarian Meal: Chilli Paneer");
+    }
+}
+
+//Vegan Meal
+class VeganMeal implements MealPlan {
+    @Override
+    public void displayMeal() {
+        System.out.println("Vegan Meal: Creamy Vegan Pasta");
+    }
+}
+
+//High Protein Meal
+class HighProteinMeal implements MealPlan {
+    @Override
+    public void displayMeal() {
+        System.out.println("High-Protein Meal: Salmon & Asparagus with Lemon-Garlic Butter Sauce");
+    }
+}
+
+
