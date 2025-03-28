ebd3af40f7b6cae162eb9083cbbe71ac85a3b5bc
Hitesh0627 <103327522+Hitesh0627@users.noreply.github.com>
2025-03-28 11:33:43 +0530

    [Hitesh] ADD: Added enum for Product Categories

---
+++ com/bridgelabz/OnlineMarketPlace/src/ProductCategories.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.OnlineMarketPlace.src;
+
+
+// Enum for Book Categories
+enum BookCategory {
+    FICTION, NON_FICTION, EDUCATIONAL
+}
+
+// Enum for Clothing Categories
+enum ClothingCategory {
+    MEN, WOMEN, KIDS
+}
+
+// Enum for Gadget Categories
+enum GadgetCategory {
+    MOBILE, LAPTOP, ACCESSORY
+}
